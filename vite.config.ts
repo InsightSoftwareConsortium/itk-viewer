d6d6598c0f387040383641f1737f127dab986ad8
Paul Elliott <paul.elliott@kitware.com>
2022-03-29 15:00:23 -0400

    feat(build): point to new entry point

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: 'lib/TfEditor.ts',
-      name: 'TfEditor',
-      fileName: (format) => `TfEditor.${format}.js`,
+      entry: 'lib/TransferFunctionEditor.ts',
+      name: 'TransferFunctionEditor',
+      fileName: (format) => `TransferFunctionEditor.${format}.js`,
     },
   },
   test: {
