6e917bbde6d953cd9142f4170136f35065f198c8
Paul Elliott <paul.elliott@kitware.com>
2022-07-19 14:54:33 -0400

    fix(WheelZoom): less strangeness when zooming out outside of border

--- lib/WheelZoom.ts
+++ lib/WheelZoom.ts
@@ -9,20 +9,26 @@ export const WheelZoom = (container: ContainerType) => {
 
     const scaleFactor = e.deltaY > 0 ? SCALE_SENSITIVITY : 1 / SCALE_SENSITIVITY
 
-    const [targetX, targetY] = container
-      .domToNormalized(e.clientX, e.clientY)
-      // clamp to avoid bug when cursor out of bounds
-      .map((coord) => Math.min(1, Math.max(0, coord)))
+    const [targetX, targetY] = container.domToNormalized(e.clientX, e.clientY)
 
     const [left, right, bottom, top] = container.getViewBox()
-    const newLeft = left - (targetX - left) * (scaleFactor - 1)
+
+    const newLeft = Math.max(
+      0,
+      left - Math.max(0, targetX - left) * (scaleFactor - 1)
+    )
     const newRight = (right - left) * scaleFactor + newLeft
-    const newBottom = bottom - (targetY - bottom) * (scaleFactor - 1)
+
+    const newBottom = Math.max(
+      0,
+      bottom - Math.max(0, targetY - bottom) * (scaleFactor - 1)
+    )
     const newTop = (top - bottom) * scaleFactor + newBottom
+
     container.setViewBox(
-      Math.max(0, newLeft),
+      newLeft,
       Math.min(1, newRight),
-      Math.max(0, newBottom),
+      newBottom,
       Math.min(1, newTop)
     )
   })
