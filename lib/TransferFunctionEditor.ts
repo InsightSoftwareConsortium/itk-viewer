4495f1b2ef2c5e62baf31fc4c6f2b5591d738541
Paul Elliott <paul.elliott@kitware.com>
2022-08-17 12:32:11 -0400

    fix(TransferFunctionEditor): setPoints did not update UI

--- lib/TransferFunctionEditor.ts
+++ lib/TransferFunctionEditor.ts
@@ -43,8 +43,11 @@ export class TransferFunctionEditor {
     return this.points.points.map(({ x, y }) => [x, y])
   }
 
+  // No Points update event on setPoints to avoid emitting 'update' event to user code.
+  // User code responsible for updating downstream piecewise function without update in setPoints case.
   setPoints(points: [number, number][]) {
     this.points.setPoints(points)
+    this.pointController.updatePoints()
   }
 
   get eventTarget() {
