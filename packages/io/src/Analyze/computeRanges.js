5a5b72949088169c61e81e9882b22c048f47e926
Paul Elliott <paul.elliott@kitware.com>
2023-04-16 13:03:47 -0400

    fix(io): use Vite syntax for workers

--- packages/io/src/Analyze/computeRanges.js
+++ packages/io/src/Analyze/computeRanges.js
@@ -1,22 +1,25 @@
-import webWorkerPromiseWorkerPool from './webWorkerPromiseWorkerPool'
-import ComputeRangesWorker from './ComputeRanges.worker'
-import { createRangeHelper } from './createRangeHelper'
-const haveSharedArrayBuffer = typeof globalThis.SharedArrayBuffer === 'function'
+import webWorkerPromiseWorkerPool from './webWorkerPromiseWorkerPool';
+// import ComputeRangesWorker from './ComputeRanges.worker';
+import { createRangeHelper } from './createRangeHelper';
+const haveSharedArrayBuffer =
+  typeof globalThis.SharedArrayBuffer === 'function';
 
 const numberOfWorkers = navigator.hardwareConcurrency
   ? Math.min(navigator.hardwareConcurrency, 8)
-  : 4
+  : 4;
 
 const computeRangeWorkerPool = webWorkerPromiseWorkerPool(
   numberOfWorkers,
-  ComputeRangesWorker,
+  new Worker(new URL('./ComputeRanges.worker.js', import.meta.url), {
+    type: 'module',
+  }),
   'computeRanges'
-)
+);
 
 export async function computeRanges(values, numberOfComponents = 1) {
-  const numberOfSplits = numberOfWorkers
+  const numberOfSplits = numberOfWorkers;
 
-  const taskArgs = new Array(numberOfSplits)
+  const taskArgs = new Array(numberOfSplits);
   if (haveSharedArrayBuffer && values.buffer instanceof SharedArrayBuffer) {
     for (let split = 0; split < numberOfSplits; split++) {
       taskArgs[split] = [
@@ -26,16 +29,16 @@ export async function computeRanges(values, numberOfComponents = 1) {
           values,
           numberOfComponents,
         },
-      ]
+      ];
     }
   } else {
-    let arrayStride = Math.floor(values.length / numberOfSplits) || 1
-    arrayStride += arrayStride % numberOfComponents
-    let arrayIndex = 0
+    let arrayStride = Math.floor(values.length / numberOfSplits) || 1;
+    arrayStride += arrayStride % numberOfComponents;
+    let arrayIndex = 0;
     for (let split = 0; split < numberOfSplits; split++) {
-      const arrayStart = arrayIndex
-      const arrayEnd = Math.min(arrayIndex + arrayStride, values.length - 1)
-      const subArray = values.slice(arrayStart, arrayEnd + 1)
+      const arrayStart = arrayIndex;
+      const arrayEnd = Math.min(arrayIndex + arrayStride, values.length - 1);
+      const subArray = values.slice(arrayStart, arrayEnd + 1);
       taskArgs[split] = [
         {
           split: 0, // 0 because array already split
@@ -44,20 +47,20 @@ export async function computeRanges(values, numberOfComponents = 1) {
           numberOfComponents,
         },
         [subArray.buffer],
-      ]
-      arrayIndex += arrayStride
+      ];
+      arrayIndex += arrayStride;
     }
   }
 
-  const rangesBySplit = await computeRangeWorkerPool.runTasks(taskArgs).promise
+  const rangesBySplit = await computeRangeWorkerPool.runTasks(taskArgs).promise;
 
-  const helpers = [...Array(numberOfComponents)].map(createRangeHelper)
+  const helpers = [...Array(numberOfComponents)].map(createRangeHelper);
   rangesBySplit.forEach(({ result: ranges }) => {
     ranges.forEach(({ min, max }, compIdx) => {
-      helpers[compIdx].add(min)
-      helpers[compIdx].add(max)
-    })
-  })
+      helpers[compIdx].add(min);
+      helpers[compIdx].add(max);
+    });
+  });
 
-  return helpers.map(h => h.getRange())
+  return helpers.map((h) => h.getRange());
 }
--- packages/viewer/src/viewer.ts
+++ packages/viewer/src/viewer.ts
@@ -1,12 +1,7 @@
-import { ZarrStoreParser } from 'itk-viewer-io/ZarrStoreParser.js';
 import { Viewport } from './viewport';
 
 export class Viewer {
   viewports: Viewport[] = [];
-
-  constructor() {
-    ZarrStoreParser;
-  }
 
   addViewport(viewport: Viewport) {
     this.viewports.push(viewport);
