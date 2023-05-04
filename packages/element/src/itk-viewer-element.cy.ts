d8163d463744dff8c822e4373365ca50f50db81d
Paul Elliott <paul.elliott@kitware.com>
2023-05-04 14:52:59 -0400

    chore(element): rename tsconfig.json to tsconfig.build.json

--- packages/element/src/itk-viewer-element.cy.ts
+++ packages/element/src/itk-viewer-element.cy.ts
@@ -4,7 +4,7 @@ import './itk-viewer-element';
 
 describe('Lit mount', () => {
   it('mounts', () => {
-    cy.mount<'itk-viewer'>(html`<itk-viewer></itk-viewer>`);
+    cy.mount<'itk-viewer'>(html`<itk-viewer image="url"></itk-viewer>`);
     cy.get('itk-viewer').shadow().contains('Viewer');
   });
 });
