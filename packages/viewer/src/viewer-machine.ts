788d1e39b1410f3ddf33d6a7bd0631323f18361c
Paul Elliott <paul.elliott@kitware.com>
2023-05-06 15:45:50 -0400

    fix(viewer-machine): add explicit type to fix build errors

--- packages/viewer/src/viewer-machine.ts
+++ packages/viewer/src/viewer-machine.ts
@@ -3,16 +3,27 @@ import { assign, createMachine } from 'xstate';
 import MultiscaleSpatialImage from '@itk-viewer/io/MultiscaleSpatialImage.js';
 import { Viewport } from './viewport.js';
 
+type addViewportEvent = {
+  type: 'addViewport';
+  name: string;
+  viewport: Viewport;
+};
+
+type addImageEvent = {
+  type: 'addImage';
+  name: string;
+  image: MultiscaleSpatialImage;
+};
+
+type context = {
+  viewports: Record<string, Viewport>;
+  images: Record<string, MultiscaleSpatialImage>;
+};
+
 export const viewerMachine = createMachine({
   types: {} as {
-    context: {
-      viewports: Record<string, Viewport>;
-      images: Record<string, MultiscaleSpatialImage>;
-    };
-
-    events:
-      | { type: 'addViewport'; name: string; viewport: Viewport }
-      | { type: 'addImage'; name: string; image: MultiscaleSpatialImage };
+    context: context;
+    events: addViewportEvent | addImageEvent;
   },
   id: 'viewer',
   initial: 'active',
@@ -25,7 +36,13 @@ export const viewerMachine = createMachine({
       on: {
         addViewport: {
           actions: assign({
-            viewports: ({ event: { name, viewport }, context }) => ({
+            viewports: ({
+              event: { name, viewport },
+              context,
+            }: {
+              event: addViewportEvent;
+              context: context;
+            }) => ({
               ...context.viewports,
               [name]: viewport,
             }),
@@ -34,12 +51,24 @@ export const viewerMachine = createMachine({
         addImage: {
           actions: [
             assign({
-              images: ({ event: { name, image }, context }) => ({
+              images: ({
+                event: { name, image },
+                context,
+              }: {
+                event: addImageEvent;
+                context: context;
+              }) => ({
                 ...context.images,
                 [name]: image,
               }),
             }),
-            ({ event: { name }, context: { images, viewports } }) => {
+            ({
+              event: { name },
+              context: { images, viewports },
+            }: {
+              event: addImageEvent;
+              context: context;
+            }) => {
               Object.values(viewports).forEach((viewport) => {
                 viewport.send({ type: 'setImage', image: images[name] });
               });
--- packages/viewer/src/viewport-machine.ts
+++ packages/viewer/src/viewport-machine.ts
@@ -2,12 +2,19 @@ import { assign, createMachine } from 'xstate';
 
 import MultiscaleSpatialImage from '@itk-viewer/io/MultiscaleSpatialImage.js';
 
+type context = {
+  image: MultiscaleSpatialImage | undefined;
+};
+
+type setImageEvent = {
+  type: 'setImage';
+  image: MultiscaleSpatialImage;
+};
+
 export const viewportMachine = createMachine({
   types: {} as {
-    context: {
-      image: MultiscaleSpatialImage | undefined;
-    };
-    events: { type: 'setImage'; image: MultiscaleSpatialImage };
+    context: context;
+    events: setImageEvent;
   },
   id: 'viewport',
   initial: 'active',
@@ -18,7 +25,7 @@ export const viewportMachine = createMachine({
         setImage: {
           actions: [
             assign({
-              image: ({ event: { image } }) => image,
+              image: ({ event: { image } }: { event: setImageEvent }) => image,
             }),
           ],
         },
