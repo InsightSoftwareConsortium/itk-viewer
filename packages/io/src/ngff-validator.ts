45a008025d4b125ae90133cee40b007c0b4762fb
Paul Elliott <paul.elliott@kitware.com>
2023-07-06 10:39:56 -0400

    refactor(ngff-validator): export Transform type, remove from ngff-types
    Add link to Schema to Zod converter

--- packages/io/src/ngff-validator.ts
+++ packages/io/src/ngff-validator.ts
@@ -1,6 +1,10 @@
-import { z } from 'zod';
+/**
+ * Zod schemas started with converter here: https://stefanterdell.github.io/json-schema-to-zod-react/
+ * then manually edited.
+ *
+ */
 
-export const IMAGE_VERSION_DEFAULT = '0.4';
+import { z } from 'zod';
 
 const direction = z.array(z.array(z.number()).length(3)).length(3);
 const ranges = z.array(z.array(z.number()).length(2));
@@ -21,24 +25,29 @@ const customNgffProperties = {
   ranges: ranges.optional(),
 };
 
-const coordinateTransformations = z
-  .array(
-    z.union([
-      z.object({
-        type: z.enum(['scale']),
-        scale: z.array(z.number()).min(2),
-      }),
-      z.object({
-        type: z.enum(['translation']),
-        translation: z.array(z.number()).min(2),
-      }),
-    ])
-  )
-  .min(1);
+const transform = z.union([
+  z.object({
+    type: z.enum(['scale']),
+    scale: z.array(z.number()).min(2),
+  }),
+  z.object({
+    type: z.enum(['translation']),
+    translation: z.array(z.number()).min(2),
+  }),
+]);
 
+export type Transform = z.infer<typeof transform>;
+
+const coordinateTransformations = z.array(transform).min(1);
+
+// if no version specified, assume this version
+export const IMAGE_VERSION_DEFAULT = '0.4';
+
+// object with version as key
 export const image = {
   '0.4': z
     .object({
+      // array of NgffImage
       multiscales: z
         .array(
           z.object({
@@ -65,8 +74,8 @@ export const image = {
             z.object({
               window: z.object({
                 end: z.number(),
-                max: z.number(),
-                min: z.number(),
+                max: z.number().optional(), // optional looser than spec
+                min: z.number().optional(), // optional looser than spec
                 start: z.number(),
               }),
               label: z.string().optional(),
@@ -123,3 +132,25 @@ export const image = {
 type Schema = z.infer<(typeof image)['0.1'] | (typeof image)['0.4']>;
 
 export type NgffImage = Schema['multiscales'][number];
+
+// Zarr schemas \\
+
+export const datasetZattrs = z.object({
+  _ARRAY_DIMENSIONS: z.array(dimension).optional(),
+  ...customNgffProperties,
+});
+
+export const zArray = z.object({
+  shape: z.array(z.number()),
+  chunks: z.array(z.number()),
+  dtype: z.string(),
+  compressor: z.object({
+    cname: z.enum(['raw', 'zlib', 'blosc', 'bzip2', 'lz4', 'lz4hc', 'zstd']),
+    blocksize: z.number().optional(),
+    clevel: z.number().optional(),
+    shuffle: z.number().optional(),
+  }),
+  dimension_separator: z.string().optional(),
+});
+
+export type ZArray = z.infer<typeof zArray>;
