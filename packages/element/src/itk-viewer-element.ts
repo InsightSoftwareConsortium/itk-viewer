8b51beaee1cd98c8fc253d339dd6ac4529e83976
Paul Elliott <paul.elliott@kitware.com>
2023-07-12 11:25:56 -0400

    feat(itk-viewer-element): Add a main.ts to drive example

--- packages/element/src/itk-viewer-element.ts
+++ packages/element/src/itk-viewer-element.ts
@@ -1,25 +1,19 @@
 import { LitElement, css, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 
-// import { ZarrMultiscaleSpatialImage } from '@itk-viewer/io/ZarrMultiscaleSpatialImage.js';
 import { Viewer, createViewer } from '@itk-viewer/viewer/viewer.js';
 import { ItkViewport } from './itk-viewport.js';
 import MultiscaleSpatialImage from '@itk-viewer/io/MultiscaleSpatialImage.js';
 
 @customElement('itk-viewer')
 export class ItkViewer extends LitElement {
-  @property()
-  imageUrl = '';
-
   viewer: Viewer;
   image?: MultiscaleSpatialImage;
-  imageInfo = '';
   viewports: ItkViewport[] = [];
 
   constructor() {
     super();
     this.viewer = createViewer();
-    this.createImage();
   }
 
   distributeImage() {
@@ -28,14 +22,8 @@ export class ItkViewer extends LitElement {
     });
   }
 
-  async createImage() {
-    // const storeURL = new URL(
-    //   '/ome-ngff-prototypes/single_image/v0.4/zyx.ome.zarr',
-    //   document.location.origin
-    // );
-    // this.image = (await ZarrMultiscaleSpatialImage.fromUrl(
-    //   storeURL
-    // )) as unknown as MultiscaleSpatialImage;
+  addImage(image: MultiscaleSpatialImage) {
+    this.image = image;
     this.distributeImage();
   }
 
--- packages/element/vite.config.ts
+++ packages/element/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  publicDir: '../../test/data/input',
   build: {
     lib: {
       entry: 'src/itk-viewer-element.ts',
