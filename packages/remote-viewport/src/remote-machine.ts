ea4ec66f3f1ea555e3789962bfa1244d19fafe74
Paul Elliott <paul.elliott@kitware.com>
2023-07-25 12:32:13 -0400

    feat(remote-viewport): add camera update initialization

--- packages/remote-viewport/src/remote-machine.ts
+++ packages/remote-viewport/src/remote-machine.ts
@@ -1,10 +1,12 @@
-import { assign, createMachine } from 'xstate';
+import { Viewport } from '@itk-viewer/viewer/viewport.js';
+import { ActorRef, assign, createMachine } from 'xstate';
 
 type Context = {
   address: string | undefined;
   server: any | undefined;
-  frame: any | undefined;
+  frame: string | undefined;
   density: number;
+  viewport: Viewport;
 };
 
 type SetAddressEvent = {
@@ -21,21 +23,38 @@ type RenderEvent = {
   type: 'render';
 };
 
+type CameraPoseUpdatedEvent = {
+  type: 'cameraPoseUpdated';
+};
+
 export const remoteMachine = createMachine({
   types: {} as {
     context: Context;
-    events: SetAddressEvent | SetDensityEvent | RenderEvent;
+    events:
+      | SetAddressEvent
+      | SetDensityEvent
+      | RenderEvent
+      | CameraPoseUpdatedEvent;
   },
   id: 'remote',
   initial: 'disconnected',
-  context: {
+  context: ({ input }: { input: { viewport: Viewport } }) => ({
     address: undefined,
     server: undefined,
     frame: undefined,
     density: 30,
-  },
+    ...input, // captures injected viewport
+  }),
   states: {
     disconnected: {
+      entry: ({ context, self }) => {
+        console.log(context);
+        context.viewport.subscribe(() => {
+          (self as ActorRef<CameraPoseUpdatedEvent>).send({
+            type: 'cameraPoseUpdated',
+          });
+        });
+      },
       on: {
         setAddress: {
           actions: [
@@ -91,6 +110,9 @@ export const remoteMachine = createMachine({
               ],
               target: 'updating',
             },
+            cameraPoseUpdated: {
+              target: 'updating',
+            },
           },
         },
         updating: {
@@ -100,6 +122,9 @@ export const remoteMachine = createMachine({
             input: ({ context }: { context: Context }) => ({
               renderer: context.server,
               density: context.density,
+              camera: context.viewport
+                ?.getSnapshot()
+                .context.camera?.getSnapshot().context,
             }),
             onDone: {
               target: 'render',
--- packages/remote-viewport/src/remote-viewport.cy.ts
+++ packages/remote-viewport/src/remote-viewport.cy.ts
@@ -1,4 +1,24 @@
-import { createRemoteViewport, createTestActors } from './remote-viewport.js';
+import { fromPromise } from 'xstate';
+import {
+  RemoteMachineActors,
+  createRemoteViewport,
+} from './remote-viewport.js';
+
+export const createTestActors: () => RemoteMachineActors = () => ({
+  actors: {
+    connect: fromPromise(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 10));
+      return 'aServer';
+    }),
+    renderer: fromPromise(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 10));
+      return { frame: 'new frame here' };
+    }),
+    updater: fromPromise(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 10));
+    }),
+  },
+});
 
 describe('remote-viewport', () => {
   it('remote actor saves server address', () => {
