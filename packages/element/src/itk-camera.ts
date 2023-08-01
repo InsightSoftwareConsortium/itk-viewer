167df422e2a58e95f5d260a2ac19ed4554954355
Paul Elliott <paul.elliott@kitware.com>
2023-07-31 22:03:11 -0400

    feat(itk-camera): wire up mouse events to camera controller

--- packages/element/src/itk-camera.ts
+++ packages/element/src/itk-camera.ts
@@ -1,10 +1,105 @@
 import { LitElement, PropertyValues, css, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { mat4 } from 'gl-matrix';
+import { ReadonlyMat4, mat4 } from 'gl-matrix';
 import createOrbitCamera from 'orbit-camera';
 
 import { Viewport } from '@itk-viewer/viewer/viewport.js';
 import { createCamera } from '@itk-viewer/viewer/camera-machine.js';
+import { Ref, createRef, ref } from 'lit/directives/ref.js';
+
+type OrbitCameraController = ReturnType<typeof createOrbitCamera>;
+
+const PAN_SPEED = 1;
+
+const bindCamera = (
+  camera: OrbitCameraController,
+  element: HTMLElement,
+  onUpdate: (view: ReadonlyMat4) => unknown
+) => {
+  let width = element.clientWidth;
+  let height = element.clientHeight;
+
+  const view = mat4.create();
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      width = entry.contentRect.width;
+      height = entry.contentRect.height;
+    }
+  });
+  resizeObserver.observe(element);
+
+  let rotate = false;
+  let pan = false;
+  let scale = false;
+
+  const onMouseDown = (e: MouseEvent) => {
+    e.preventDefault();
+    if (e.button === 0) {
+      rotate = true;
+    } else if (e.button === 1) {
+      scale = true;
+    } else if (e.button === 2) {
+      pan = true;
+    }
+  };
+  element.addEventListener('mousedown', onMouseDown);
+
+  const onMouseUp = (e: MouseEvent) => {
+    if (e.button === 0) {
+      rotate = false;
+    } else if (e.button === 1) {
+      scale = false;
+    } else if (e.button === 2) {
+      pan = false;
+    }
+  };
+  element.addEventListener('mouseup', onMouseUp);
+
+  let prevMouseX = 0;
+  let prevMouseY = 0;
+
+  const onMouseMove = (e: MouseEvent) => {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+
+    if (rotate) {
+      camera.rotate(
+        [mouseX / width - 0.5, mouseY / height - 0.5],
+        [prevMouseX / width - 0.5, prevMouseY / height - 0.5]
+      );
+    }
+
+    if (pan) {
+      camera.pan([
+        (PAN_SPEED * (mouseX - prevMouseX)) / width,
+        (PAN_SPEED * (mouseY - prevMouseY)) / height,
+      ]);
+    }
+
+    if (scale) {
+      const d = mouseY - prevMouseY;
+      if (d) camera.distance *= Math.exp(d / height);
+    }
+
+    prevMouseX = mouseX;
+    prevMouseY = mouseY;
+
+    if (!rotate && !pan && !scale) return;
+    camera.view(view);
+    onUpdate(view);
+  };
+  element.addEventListener('mousemove', onMouseMove);
+
+  const unbind = () => {
+    resizeObserver.disconnect();
+    element.removeEventListener('mousemove', onMouseMove);
+    element.removeEventListener('mousedown', onMouseDown);
+    element.removeEventListener('mouseup', onMouseUp);
+  };
+
+  return unbind;
+};
 
 @customElement('itk-camera')
 export class ItkCamera extends LitElement {
@@ -12,35 +107,30 @@ export class ItkCamera extends LitElement {
   viewport: Viewport | undefined;
 
   camera = createCamera();
+  unbind: (() => unknown) | undefined;
+  container: Ref<HTMLElement> = createRef();
 
-  constructor() {
-    super();
-    const view = mat4.create();
+  firstUpdated(): void {
     const cameraController = createOrbitCamera(
-      [0, 0, 1],
+      [-0.747528, -0.570641, 0.754992],
       [0.5, 0.5, 0.5],
-      [0, 1, 0]
+      [-0.505762, 0.408327, -0.759916]
     );
 
-    const prevMouse = [0, 0];
+    const container = this.container.value;
+    if (!container) throw new Error('container not found');
 
-    const shell = { width: 500, height: 400 };
-
-    this.addEventListener('mousemove', (e: MouseEvent) => {
-      const [mouseX, mouseY] = [e.clientX, e.clientY];
-      const [prevMouseX, prevMouseY] = prevMouse;
-
-      cameraController.rotate(
-        [mouseX / shell.width - 0.5, mouseY / shell.height - 0.5],
-        [prevMouseX / shell.width - 0.5, prevMouseY / shell.height - 0.5]
-      );
-
-      cameraController.view(view);
+    this.unbind = bindCamera(cameraController, container, (view) => {
       this.camera.send({
         type: 'setPose',
         pose: view,
       });
     });
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this.unbind?.();
   }
 
   willUpdate(changedProperties: PropertyValues<this>) {
@@ -50,7 +140,11 @@ export class ItkCamera extends LitElement {
   }
 
   render() {
-    return html`<div class="container"><slot></slot></div>`;
+    return html`
+      <div class="container" ${ref(this.container)}>
+        <slot></slot>
+      </div>
+    `;
   }
 
   static styles = css`
--- packages/remote-viewport/src/remote-viewport.ts
+++ packages/remote-viewport/src/remote-viewport.ts
@@ -18,7 +18,7 @@ const createHyphaRenderer = async (server_url: string) => {
     server_url,
   };
   const server = await hyphaWebsocketClient.connectToServer(config);
-  const renderer = await server.getService('test-agave-renderer');
+  const renderer = await server.getService('test-agave-renderer-paul');
   await renderer.setup();
   await renderer.setImage('data/aneurism.ome.tif');
   return renderer;
