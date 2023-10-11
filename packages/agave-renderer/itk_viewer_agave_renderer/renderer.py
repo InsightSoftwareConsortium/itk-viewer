f6c0e9601e24a02c5011b3cd140d79d1ca359e5d
Paul Elliott <paul.elliott@kitware.com>
2023-10-11 17:54:30 -0400

    refactor(renderer): format with black

--- packages/agave-renderer/itk_viewer_agave_renderer/renderer.py
+++ packages/agave-renderer/itk_viewer_agave_renderer/renderer.py
@@ -13,6 +13,7 @@ from itkwasm import Image, ImageType, PixelTypes, IntTypes
 import numpy as np
 from PIL import Image as PILImage
 
+
 class AgaveRendererMemoryRedraw(agave.AgaveRenderer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -27,8 +28,9 @@ class AgaveRendererMemoryRedraw(agave.AgaveRenderer):
         # ready for next frame
         self.cb = agave.commandbuffer.CommandBuffer()
         img = PILImage.open(binarydata)
-        rgba = img.convert('RGBA').tobytes()
+        rgba = img.convert("RGBA").tobytes()
         return rgba
+
 
 # how to handle unknown events
 class UnknownEventAction(Enum):
@@ -36,8 +38,11 @@ class UnknownEventAction(Enum):
     WARNING = auto()
     IGNORE = auto()
 
-class Renderer():
-    def __init__(self, width=500, height=400, unknown_event_action=UnknownEventAction.WARNING):
+
+class Renderer:
+    def __init__(
+        self, width=500, height=400, unknown_event_action=UnknownEventAction.WARNING
+    ):
         self.width = width
         self.height = height
         self.unknown_event_action = unknown_event_action
@@ -56,40 +61,45 @@ class Renderer():
         r = self.agave
         start_time = time.time()
         rgba = r.memory_redraw()
-        image_type = ImageType(dimension=2, componentType=IntTypes.UInt8, pixelType=PixelTypes.RGBA, components=4)
+        image_type = ImageType(
+            dimension=2,
+            componentType=IntTypes.UInt8,
+            pixelType=PixelTypes.RGBA,
+            components=4,
+        )
         image = Image(image_type)
         image.size = [self.width, self.height]
         image.data = np.frombuffer(rgba, dtype=np.uint8)
         # rgba_encoded = encode(image) # lossless
         rgba_encoded = encode(image, not_reversible=True, quantization_step=0.02)
         elapsed = time.time() - start_time
-        return { "frame": rgba_encoded, "renderTime": elapsed }
+        return {"frame": rgba_encoded, "renderTime": elapsed}
 
     async def update_renderer(self, events):
         r = self.agave
 
         for [event_type, payload] in events:
-           match event_type:
-                case 'loadImage':
+            match event_type:
+                case "loadImage":
                     await self.load_image(**payload)
-                case 'cameraPose':
-                   eye = payload['eye']
-                   r.eye(eye[0], eye[1], eye[2])
-                   up = payload['up']
-                   r.up(up[0], up[1], up[2])
-                   target = payload['center']
-                   r.target(target[0], target[1], target[2])
-                case 'renderSize':
+                case "cameraPose":
+                    eye = payload["eye"]
+                    r.eye(eye[0], eye[1], eye[2])
+                    up = payload["up"]
+                    r.up(up[0], up[1], up[2])
+                    target = payload["center"]
+                    r.target(target[0], target[1], target[2])
+                case "renderSize":
                     self.set_render_size(payload[0], payload[1])
-                case 'density':
+                case "density":
                     r.density(payload)
-                case 'renderIterations':
-                   r.render_iterations(payload)
+                case "renderIterations":
+                    r.render_iterations(payload)
                 case _:
                     self.handle_unknown_event(event_type)
 
     def handle_unknown_event(self, event_type):
-       match self.unknown_event_action:
+        match self.unknown_event_action:
             case UnknownEventAction.ERROR:
                 raise Exception(f"Unknown event type: {event_type}")
             case UnknownEventAction.WARNING:
@@ -97,30 +107,35 @@ class Renderer():
             case UnknownEventAction.IGNORE:
                 pass
 
-    async def connect(self, hypha_server_url, load_image_into_agave_fn, visibility="public", identifier="agave-renderer"):
+    async def connect(
+        self,
+        hypha_server_url,
+        load_image_into_agave_fn,
+        visibility="public",
+        identifier="agave-renderer",
+    ):
         server = await connect_to_server(
             {
-              "name": "agave-renderer-client",
-              "server_url": hypha_server_url
+                "name": "agave-renderer-client",
+                "server_url": hypha_server_url,
             }
         )
 
         self.load_image = functools.partial(load_image_into_agave_fn, self)
 
-        await server.register_service({
-            "name": "Agave Renderer",
-            "id": identifier,
-            "config": {
-                "visibility": visibility,
-                "require_context": False,
-                "run_in_executor": False,
-            },
-
-            "setup": self.setup,
-
-            "loadImage": self.load_image,
-
-            "render": self.render,
-            "updateRenderer": self.update_renderer,
-        })
-        print("Renderer is ready to receive request!",  server.config, flush=True)
+        await server.register_service(
+            {
+                "name": "Agave Renderer",
+                "id": identifier,
+                "config": {
+                    "visibility": visibility,
+                    "require_context": False,
+                    "run_in_executor": False,
+                },
+                "setup": self.setup,
+                "loadImage": self.load_image,
+                "render": self.render,
+                "updateRenderer": self.update_renderer,
+            }
+        )
+        print("Renderer is ready to receive request!", server.config, flush=True)
