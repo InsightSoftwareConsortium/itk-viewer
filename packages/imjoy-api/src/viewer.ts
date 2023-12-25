3745e4f3b3b18a62872f535d377c2a9293d40122
Brianna Major <brianna.major@nibeko.khq.kitware.com>
2023-12-25 18:01:10 -0500

    feat(imjoy-api): Add wrappers to send events to the viewer machine.

--- packages/imjoy-api/src/viewer.ts
+++ packages/imjoy-api/src/viewer.ts
@@ -1,7 +1,22 @@
 import { Viewer } from '@itk-viewer/viewer/viewer.js';
+import { Viewport } from '@itk-viewer/viewer/viewport.js';
+import MultiscaleSpatialImage from '@itk-viewer/io/MultiscaleSpatialImage.js';
 
-type HyphaServer = unknown;
-
-export const setup = (server: HyphaServer, viewer: Viewer) => {
-  console.log('setup', { server, viewer });
+type HyphaServer = {
+  registerService: (api: object) => any,
+  getService: (name: string) => any,
+  id: string,
 };
+
+export async function setup(server: HyphaServer, viewer: Viewer) {
+  const service = await server.registerService({
+    name: "itk_viewer",
+    id: "itk-viewer",
+    description: "Retrieve the viewer API",
+    viewer: () => viewer,
+    addViewport: (viewport: Viewport, name: string) => viewer.send({ type: 'addViewport', viewport: viewport, name: name }),
+    setImage: (image: MultiscaleSpatialImage, name?: string) => viewer.send({ type: 'setImage', image, name: name ?? 'image' }),
+  });
+
+  return await server.getService(service.id)
+}
