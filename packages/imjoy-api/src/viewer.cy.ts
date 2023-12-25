3b6c23c980a372f33a476fcaf5e87220db603639
Brianna Major <brianna.major@nibeko.khq.kitware.com>
2023-12-25 18:01:10 -0500

    feat(imjoy-api): Add wrappers to send events to the viewer machine.

--- packages/imjoy-api/src/viewer.cy.ts
+++ packages/imjoy-api/src/viewer.cy.ts
@@ -1,21 +1,72 @@
-import { createViewer } from '@itk-viewer/viewer/viewer.js';
 import { hyphaWebsocketClient } from 'imjoy-rpc';
 import { setup } from './viewer.js';
+import { createViewer } from '@itk-viewer/viewer/viewer.js';
+import { createViewport } from '@itk-viewer/viewer/viewport.js';
+import { ZarrMultiscaleSpatialImage } from '@itk-viewer/io/ZarrMultiscaleSpatialImage.js';
 
-const serverUrl = 'http://localhost:9000';
+const serverUrl = 'http://localhost:37580';
 
 describe('imjoy-viewer', () => {
-  it('registers service with Hypha server', async () => {
+  it('registers service with Hypha server', () => {
+    cy.wrap(hyphaWebsocketClient.connectToServer({
+      name: 'test-client',
+      server_url: serverUrl,
+    })).then((server) => {
+      const viewer = createViewer();
+      cy.wrap(setup(server, viewer)).should('be.ok')
+      .and('have.all.keys',
+      'addViewport',
+      'config',
+      'description',
+      'id',
+      'name',
+      'setImage',
+      'type',
+      'viewer');
+    });
+  });
+
+  it('returns the viewer', () => {
     const viewer = createViewer();
 
-    // TODO? beforeEach spin up hypha signaling server
-    const server = await hyphaWebsocketClient.connectToServer({
-      name: 'remote-renderer-client',
+    cy.wrap(hyphaWebsocketClient.connectToServer({
+      name: 'test-client',
       server_url: serverUrl,
-    });
-
-    const service = setup(server, viewer);
-
-    expect(service).to.be.ok;
+    })).then((server) => {
+      return cy.wrap(setup(server, viewer))
+    })
+    .then((service) => {
+      cy.wrap(service.viewer()).should('equal', viewer);
+    });;
   });
-});
+
+  it('accepts a viewport and set image', () => {
+    const viewer = createViewer();
+    const viewport = createViewport();
+    const storeURL = new URL(
+      '/ome-ngff-prototypes/single_image/v0.4/yx.ome.zarr',
+      document.location.origin,
+    );
+
+    cy.wrap(hyphaWebsocketClient.connectToServer({
+      name: 'test-client',
+      server_url: serverUrl,
+    }))
+    .then((server) => {
+      cy.wrap(setup(server, viewer));
+    }).then((service) => {
+      cy.wrap(service.addViewport(viewport, 'first')).then(() => {
+        cy.wrap(viewer.getSnapshot().context.viewports)
+        .should('have.property', 'first', viewport);
+      });
+      return service
+    }).then((service) => {
+      cy.wrap(ZarrMultiscaleSpatialImage.fromUrl(storeURL))
+      .then((image) => {
+        cy.wrap(service.setImage(image, 'zarr')).then(() => {
+          cy.wrap(viewport.getSnapshot().context.image).should('equal', image);
+        });
+      })
+    });
+  });
+})
