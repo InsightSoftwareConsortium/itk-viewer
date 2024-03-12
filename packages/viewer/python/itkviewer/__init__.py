7b953def4908fb658feb79aadbce0caa02b825dd
Matt McCormick <matt.mccormick@kitware.com>
2024-03-11 20:43:06 -0400

    feat(itkviewer): add Viewport, MultiscaleImage

--- packages/viewer/python/itkviewer/__init__.py
+++ packages/viewer/python/itkviewer/__init__.py
@@ -3,7 +3,11 @@
 __version__ = "0.0.1"
 
 from .viewer import Viewer
+from .viewport import Viewport
+from .multiscale_image import MultiscaleImage
 
 __all__ = [
+  "MultiscaleImage",
   "Viewer",
+  "Viewport",
   ]
