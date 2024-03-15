557ff7931101fd325987275bfb078d7274a98e11
Matt McCormick <matt.mccormick@kitware.com>
2024-03-14 23:32:46 -0400

    feat(model): add viewport to Renderer

--- packages/clara-viz/itkviewer_clara_viz/renderer.py
+++ packages/clara-viz/itkviewer_clara_viz/renderer.py
@@ -1,3 +1,7 @@
+from itkviewer import Viewport, Renderer
 
-class Renderer:
-    pass
+class ClaraVizRenderer(Renderer):
+    """itk-viewer renderer that renders images using ClaraViz."""
+
+    def __init__(self, viewport: Viewport, width: int=640, height: int=480):
+        super().__init__(viewport, width, height)
