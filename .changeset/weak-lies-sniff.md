0871fbb746f8404f9d5fbcb0d732ab542bc2750a
Paul Elliott <paul.elliott@kitware.com>
2024-07-11 18:43:03 -0400

    feat(io): expose itk-wasm setPipelineBaseUrl setPipelineWorkerUrl

---
+++ .changeset/weak-lies-sniff.md
@@ -0,0 +1,6 @@
+---
+'@itk-viewer/blosc-zarr': minor
+'@itk-viewer/io': minor
+---
+
+Export itk-wasm pipeline url setting functions.
--- packages/blosc-zarr/bloscZarrDecompress.d.ts
+++ packages/blosc-zarr/bloscZarrDecompress.d.ts
@@ -1,2 +1,7 @@
 
 export function bloscZarrDecompress(chunkData: Array<{data: ArrayBuffer, metadata: any}>): Promise<Array<ArrayBuffer>>;
+
+export function getPipelinesBaseUrl(): string;
+export function setPipelinesBaseUrl(url: string): void;
+export function getPipelineWorkerUrl(): string;
+export function setPipelineWorkerUrl(url: string): void;
