9c4ad20f52124dcc91d38fcdeedbb004df323e1a
Paul Elliott <paul.elliott@kitware.com>
2024-07-13 13:10:44 -0400

    fix(blosc-zarr): fix default pipelines path
    Did not have /emscripten-build at end of default url.
    
    Generate version file from package.json at build time.

--- packages/blosc-zarr/pipeline-urls.js
+++ packages/blosc-zarr/pipeline-urls.js
@@ -1,41 +1,34 @@
-import { getPipelinesBaseUrl as itkWasmGetPipelinesBaseUrl, getPipelineWorkerUrl as itkWasmGetPipelineWorkerUrl } from "itk-wasm";
-import { version } from "./version.js";
+import {
+  getPipelinesBaseUrl as itkWasmGetPipelinesBaseUrl,
+  getPipelineWorkerUrl as itkWasmGetPipelineWorkerUrl,
+} from 'itk-wasm';
+import { version } from './version.js';
 
-let pipelinesBaseUrl
-let defaultPipelinesBaseUrl  =
-  `https://cdn.jsdelivr.net/npm/@itk-viewer/blosc-zarr@${version}/`;
+let pipelinesBaseUrl;
+let defaultPipelinesBaseUrl = `https://cdn.jsdelivr.net/npm/@itk-viewer/blosc-zarr@${version}/emscripten-build`;
 
-export function setPipelinesBaseUrl(baseUrl)  {
+export function setPipelinesBaseUrl(baseUrl) {
   pipelinesBaseUrl = baseUrl;
 }
 
 export function getPipelinesBaseUrl() {
-  if (typeof pipelinesBaseUrl !== "undefined") {
-    return pipelinesBaseUrl;
-  }
-  const itkWasmPipelinesBaseUrl = itkWasmGetPipelinesBaseUrl();
-  if (typeof itkWasmPipelinesBaseUrl !== "undefined") {
-    return itkWasmPipelinesBaseUrl;
-  }
-  return defaultPipelinesBaseUrl;
+  return (
+    pipelinesBaseUrl ?? itkWasmGetPipelinesBaseUrl() ?? defaultPipelinesBaseUrl
+  );
 }
 
-let pipelineWorkerUrl
+let pipelineWorkerUrl;
 // Use the version shipped with an app's bundler
-const defaultPipelineWorkerUrl = null
+const defaultPipelineWorkerUrl = null;
 
-export function setPipelineWorkerUrl (workerUrl)  {
-  pipelineWorkerUrl = workerUrl
+export function setPipelineWorkerUrl(workerUrl) {
+  pipelineWorkerUrl = workerUrl;
 }
 
-export function getPipelineWorkerUrl () {
-  if (typeof pipelineWorkerUrl !== 'undefined') {
-    return pipelineWorkerUrl
-  }
-  const itkWasmPipelineWorkerUrl = itkWasmGetPipelineWorkerUrl()
-  if (typeof itkWasmPipelineWorkerUrl !== 'undefined') {
-    return itkWasmPipelineWorkerUrl
-  }
-  return defaultPipelineWorkerUrl
+export function getPipelineWorkerUrl() {
+  return (
+    pipelineWorkerUrl ??
+    itkWasmGetPipelineWorkerUrl() ??
+    defaultPipelineWorkerUrl
+  );
 }
-
